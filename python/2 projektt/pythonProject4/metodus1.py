8e44497defed720c3ecc6c3c3dce81df0eb12c07
VorosDominik <medoxidhun@gmail.com>
2023-01-04 19:00:11 +0100

    python képek plusz feladatok 1/2 része

---
+++ python/2 projektt/pythonProject4/metodus1.py
@@ -0,0 +1,30 @@
+ feladat3():
+    for i in range(21):
+        print(i)
+ feladat4():
+    for i in range(20,56,2):
+        print(i)
+ feladat5():
+    for i in range(77,-77,-2):
+        i-=1
+        print(i)
+ feladat6():
+    a =int(input("a="))
+    b = int(input("b="))
+    if a<b:
+        for i in range(a, b, 1 ):
+            i -= 1
+            print(i)
+    else:
+        for i in range(b, a, 1 ):
+            i -= 1
+            print(i)
+ feladat7():
+    a =int(input("a="))
+    b = int(input("b="))
+    c = a*b
+    for i in range(c):
+        print(i)
+
+
+
